0f3aede0be7dcc5f5bc3ac214b94c0988eb59689
Sarah-Fisher <sarah.fisher@mygeorgian.ca>
2023-07-20 19:36:14 -0400

    add new ice cream to json

--- js/json.js
+++ js/json.js
@@ -19,7 +19,7 @@ async function populate() {
     // STEP 9a: Invoke the populateHeader function here, then build it below
     populateHeader(iScream);
     // STEP 10a: Invoke the showTopFlavors function here, then build it below
-    //showTopFlavours(iScream);
+    showTopFlavors(iScream);
 }
 
 // STEP 3b: Call the populate() function
@@ -34,24 +34,49 @@ function populateHeader(jsonObj) {
     header.appendChild(h1);
 };
 /* STEP 10b: Assemble the showTopFlavors() function */
-//function showTopFlavors(jsonObj) {
+function showTopFlavors(jsonObj) {
     // STEP 10c: Bind the JSON topFlavors object to a var
-
+    let flavours = jsonObj.topFlavours;
+    console.log(flavours);
     // STEP 10d: Loop through the topFlavors object
-
+    for (let i = 0; i < flavours.length; i++){
+        console.log(flavours[i].name);
+    
     // STEP 10e: build HTML elements for the content: article, h2, image, p1, p2, list
-
+        const article = document.createElement("article");
+        const h2 = document.createElement("h2");
+        const p1 = document.createElement("p");
+        const p2 = document.createElement("p");
+        const list = document.createElement("ul");
+        const image = document.createElement("img");
 
     // STEP 10f: Set the textContent property for each of the above elements (except the UL), based on the JSON content
-
-
+        h2.textContent = flavours[i].name;
+        p1.textContent = `Calories: ${flavours[i].calories}`;
+        p2.textContent = `Type: ${flavours[i].type}`;
+        image.setAttribute("src", `images/${flavours[i].image}`);
     // STEP 10g: Build a loop for the ingredients array in the JSON
-
+        for (let j = 0; j < flavours[i].ingredients.length; j++) {
+            console.log(flavours[i].ingredients[j]);
+            const listItem = document.createElement("li");
+            //append li to ul
+            listItem.textContent = flavours[i].ingredients[j];
+            list.appendChild(listItem);
+        }
 
     // STEP 10i: Append each complete ARTICLE element to the SECTION element
-//};
+        article.appendChild(h2);
+        article.appendChild(p1);
+        article.appendChild(p2);
+        article.appendChild(image);
+        article.appendChild(list);
+        section.appendChild(article);
+
+    }
+};
 
 // STEP 11: Add a 3rd flavour of ice cream to the local JSON file, making use of the /images/strawberry-sprinkle.svg image
+
 
 // Lab: Extend the JavaScript application built in class to include two more flavors of ice cream.
 
